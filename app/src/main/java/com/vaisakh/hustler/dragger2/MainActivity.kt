555a4bad9e4429590f2bb26b7e0d2b133fb9714b
WhiteQube <vaisakhkmohan@gmail.com>
2021-03-26 02:02:56 -0700

    Update MainActivity.kt

--- app/src/main/java/com/vaisakh/hustler/dragger2/MainActivity.kt
+++ app/src/main/java/com/vaisakh/hustler/dragger2/MainActivity.kt
@@ -6,14 +6,19 @@ import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
     
+    //Field dependancy injection
      @Inject
     lateinit var car: Car
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Field dependancy injection
         var fieldinject = DaggerCarComponent.create()
         fieldinject.fieldinject(this)
+        
+        //DaggerCarComponent.create().getcar() : constructor dependancy injection
+        
         car.start()
     }
 
